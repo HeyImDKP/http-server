dd8af69d4f40178f31abfaf2468ab6f1bee1cdef
Dave Kevin Prawiro <davekevinprawiro.official@gmail.com>
2024-06-21 16:55:55 +0800

    Added front end for sign up

--- scripts/login.js
+++ scripts/login.js
@@ -1,25 +1,35 @@
 // Get the modal
-var modal = document.getElementById("id01");
+var modal = document.getElementById("idLogin");
+var modal2 = document.getElementById("idSignup");
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    } else if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 };
 
-const close_modal = () => {
-    document.getElementById("id01").style.display = "none";
+const close_modal = (id) => {
+    document.getElementById(id).style.display = "none";
     clear_login();
+    clear_signup();
 };
 
 const clear_login = () => {
-    document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
+    document.getElementById("usernameidLogin").value = "";
+    document.getElementById("passwordidLogin").value = "";
 };
 
-const show_password = () => {
-    let password = document.getElementById("password");
+const clear_signup = () => {
+    document.getElementById("usernameidSignup").value = "";
+    document.getElementById("passwordidSignup").value = "";
+    document.getElementById("emailidSignup").value = "";
+};
+
+const show_password = (id) => {
+    let password = document.getElementById("password"+id);
     if (password.type === "password") {
         password.type = "text";
     } else {
